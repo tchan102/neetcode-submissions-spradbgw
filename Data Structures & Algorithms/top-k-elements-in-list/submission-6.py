340d9718440a6a313b963028a954f5be735f423a
Tat Chan <77873239+tchan102@users.noreply.github.com>
2026-04-09 08:28:27 -0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # input: array of int, integer occurance from 1 to len(nums), integer k -> the max k occrrence integer from nums 
+        # output: a list of max k integer of most occurence integer
+
+        count = {} # n from nums -> count
+        res = []
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        count = dict(sorted(count.items(),key=lambda item: item[1], reverse=True))
+        for key, val in count.items():
+            if k == len(res): break
+            res.append(key)
+
+
+        return res
